e115496c23d45a7dc59fecf5d6b73638f7cfda5a
reloadoskar <reloadoskar@gmail.com>
2024-08-13 21:30:52 -0600

    lista de clientes updated css dark y light

--- src/app/(sistema)/clientes/ClientesList.jsx
+++ src/app/(sistema)/clientes/ClientesList.jsx
@@ -10,7 +10,7 @@ export default function ClientesList({ clientes }) {
         <>
             <div className="overflow-auto rounded-lg hidden md:block">
                 <table className="w-full ">
-                    <thead className="bg-gray-950 border-b border-gray-800">
+                    <thead className="bg-gray-400 dark:bg-gray-950 border-b dark:border-gray-800">
                         <tr>
                             <th className="px-3 text-sm tracking-wide text-left hidden md:table-cell whitespace-nowrap">Nombre</th>
                             <th className="px-3 text-sm tracking-wide text-left hidden md:table-cell whitespace-nowrap">Clave</th>
@@ -29,42 +29,42 @@ export default function ClientesList({ clientes }) {
                             .sort((a, b) => a.cuentas.reduce((ttl, cnt) => ttl += cnt.saldo, 0) > b.cuentas.reduce((ttl, cnt) => ttl += cnt.saldo, 0) ? -1 : 1)
                             .map(cliente => (
                                 <tr key={cliente._id}
-                                    className="odd:bg-gray-700 cursor-pointer hover:bg-gray-600 flex md:table-row md:flex-row flex-wrap md:flex-no-wrap py-5 md:mb-0"
+                                    className="odd:bg-gray-300 dark:odd:bg-gray-700 cursor-pointer hover:bg-gray-600 flex md:table-row md:flex-row flex-wrap md:flex-no-wrap py-5 md:mb-0"
                                     onClick={() => selectCliente(cliente)}>
-                                    <td className="px-3 w-60 text-gray-300 whitespace-nowrap">
+                                    <td className="px-3 w-60 dark:text-gray-300 whitespace-nowrap">
                                         <div className="md:hidden text-xs font-bold text-gray-500">Nombre</div>
                                         {cliente.nombre}
                                     </td>
-                                    <td className="px-3 text-gray-300  ">
-                                        <div className="md:hidden text-xs font-bold text-gray-500">Clave</div>
+                                    <td className="px-3 dark:text-gray-300  ">
+                                        <div className="md:hidden text-xs font-bold dark:text-gray-500">Clave</div>
                                         {cliente.clave}
                                     </td>
-                                    <td className="px-3 text-gray-300 whitespace-nowrap">
-                                        <div className="md:hidden text-xs font-bold text-gray-500">Referencia</div>
+                                    <td className="px-3 dark:text-gray-300 whitespace-nowrap">
+                                        <div className="md:hidden text-xs font-bold dark:text-gray-500">Referencia</div>
                                         {cliente.ref}
                                     </td>
-                                    <td className="px-3 text-gray-300 ">
-                                        <div className="md:hidden text-xs font-bold text-gray-500">email</div>
+                                    <td className="px-3 dark:text-gray-300 ">
+                                        <div className="md:hidden text-xs font-bold dark:text-gray-500">email</div>
                                         {cliente.email}
                                     </td>
-                                    <td className="px-3 text-gray-300  ">
-                                        <div className="md:hidden text-xs font-bold text-gray-500">Teléfono</div>
+                                    <td className="px-3 dark:text-gray-300  ">
+                                        <div className="md:hidden text-xs font-bold dark:text-gray-500">Teléfono</div>
                                         {cliente.tel1}
                                     </td>
-                                    <td className="px-3 text-gray-300  ">
-                                        <div className="md:hidden text-xs font-bold text-gray-500">Ubicación</div>
+                                    <td className="px-3 dark:text-gray-300  ">
+                                        <div className="md:hidden text-xs font-bold dark:text-gray-500">Ubicación</div>
                                         {cliente.ubicacion?.nombre}
                                     </td>
-                                    <td className="px-3 text-gray-300 text-right ">
-                                        <div className="md:hidden text-xs font-bold text-gray-500">Límite de crédito</div>
+                                    <td className="px-3 dark:text-gray-300 text-right ">
+                                        <div className="md:hidden text-xs font-bold dark:text-gray-500">Límite de crédito</div>
                                         {cliente.limite_de_credito}
                                     </td>
-                                    <td className="px-3 text-gray-300 text-right ">
-                                        <div className="md:hidden text-xs font-bold text-gray-500">Crédito disponible</div>
+                                    <td className="px-3 dark:text-gray-300 text-right ">
+                                        <div className="md:hidden text-xs font-bold dark:text-gray-500">Crédito disponible</div>
                                         {cliente.credito_disponible}
                                     </td>
-                                    <td className="px-3 text-gray-300 text-right ">
-                                        <div className="md:hidden text-xs font-bold text-gray-500">Saldo</div>
+                                    <td className="px-3 dark:text-gray-300 text-right ">
+                                        <div className="md:hidden text-xs font-bold dark:text-gray-500">Saldo</div>
                                         {cliente.cuentas.reduce((ttl, cnt) => ttl += cnt.saldo, 0)}
                                     </td>
                                 </tr>
